7bc9351f6802d0f137ee1fbf46298a70b93ae960
Dzmitry Vasekin <dimitva92@gmail.com>
2022-07-26 16:55:52 +0300

    verstka_card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div style={{display: 'none'}} className="overlay">
+        <div className="drawer">
+          <h2 className="d-flex justify-between mb-30">Корзина 
+          <img className="cu-p" src="/img/btn-remove.svg" alt="Remove" />
+          </h2>
+
+          <div className="items">
+          <div className="cartItem d-flex align-center mb-20">
+            <div style={{backgroundImage:'url(/imgsneakers/1.jpg)'}} className="cartItemImg"></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+                <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+            </div>
+            <div className="cartItem d-flex align-center mb-20">
+            <div style={{backgroundImage:'url(/imgsneakers/1.jpg)'}} className="cartItemImg"></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+                <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+            </div>
+          </div>
+
+          <div className="cartTotalBlock">
+          <ul className="cartTotalBlock">
+            <li className="d-flex">
+              <span>Итого: </span>
+              <div></div>
+              <b>21 498 руб. </b>
+            </li>
+            <li className="d-flex">
+              <span>Налог 5%: </span>
+              <div></div>
+              <b>1074 руб. </b>
+            </li>
+          </ul>
+          <button className="greenButton">Оформить заказ <img src="/img/Arrow.svg" alt="Arrow" /></button>
+          </div>
+          </div>
+      </div>
+
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png"/>
