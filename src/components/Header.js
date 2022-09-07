03cab16675a84637d1f1403df5bae91c7724c070
Dzmitry Vasekin <dimitva92@gmail.com>
2022-09-07 13:02:09 +0300

    main

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header(props){
         <header className="d-flex justify-between align-center p-40">
           <Link to="/">
         <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" alt='biba'/>
+          <img width={40} height={40} src="img/logo.png" alt='biba'/>
           <div>
             <h3 className="text-uppercase">react sneakers</h3>
             <p className="opacity-5">Магазин лучший кроссовок</p>
@@ -18,7 +18,7 @@ function Header(props){
         </Link>
         <ul className="d-flex">
           <li onClick={props.onClickCart} className="mr-30 cu-p">
-          <img width={18} height={18} src="/img/cart.png" alt="Корзина"/>
+          <img width={18} height={18} src="img/cart.png" alt="Корзина"/>
             <span>{totalPrice} руб.</span>
           </li>
             <Link to="/favorites">
