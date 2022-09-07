750f5012c347ca7f3ecbb301c68aceb1598dd17b
Dzmitry Vasekin <dimitva92@gmail.com>
2022-09-07 15:08:15 +0300

    correct_2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,7 @@ function Home({
         <div className="d-flex align-center justify-between mb-40">
           <h1>{searchValue ? `Поиск по запросу: "${searchValue}"`: 'Все кроссовки'}</h1>
           <div className="search-block d-flex">
-            <img src="\img\Search.svg" alt="Search"/>
+            <img src="img\Search.svg" alt="Search"/>
             {/* Кнопка очистки */}
             {searchValue && <img onClick={() => setSearchValue('')} className="clear cu-p" src="img/btn-remove.svg" alt="Clear" />}
             <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..." />
